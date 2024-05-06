4f97aa5bd9136c5d8963005fbc51ce14d3e291c7
dnzhou <zhou.danny@hotmail.com>
2024-05-07 05:45:39 +1000

    added functionality now with images and desc on popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
-function showPopup(title, description, event) {
+function showPopup(title, subtitle, description, imageSrc, event) {
+    // Prevent the default action of the event (e.g., following a link)
     event.preventDefault();
+
+    // Get the popup elements and set their content dynamically
     var popup = document.getElementById('popup');
     var popupTitle = document.getElementById('popup-title');
+    var popupSubtitle = document.getElementById('popup-subtitle');
     var popupDescription = document.getElementById('popup-description');
+    var popupImage = document.getElementById('popup-image');
 
     popupTitle.textContent = title;
+    popupSubtitle.textContent = subtitle;
     popupDescription.textContent = description;
+    popupImage.src = imageSrc;
+
+    // Display the popup
+    // Your code to display the popup goes here
     popup.style.display = 'block';
+    document.body.classList.add('popup-open'); // Add class to body
 }
 
 function closePopup() {
